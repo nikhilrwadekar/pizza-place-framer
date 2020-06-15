3da596052faa8eb9cabed2227134d4b0ae350d00
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-06-14 17:06:46 -0700

    Added Modal + Backdrop

---
+++ pizzajoint/src/components/Modal.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+
+// Variants
+const backdrop = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const Modal = ({ showModal, setShowModal }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdrop}
+          initial="hidden"
+          animate="visible"
+        ></motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Modal;
--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 // Variant
@@ -40,7 +40,15 @@ const childVariants = {
   },
 };
 
-const Order = ({ pizza }) => {
+const Order = ({ pizza, setShowModal }) => {
+  // Run once
+  useEffect(() => {
+    setTimeout(() => {
+      // Starts the timer even if the tab isn't active.
+      setShowModal(true);
+    }, 5000);
+  }, [setShowModal]);
+
   return (
     <motion.div
       variants={containerVariants}
